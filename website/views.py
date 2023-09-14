65e3529281c838ff7b8fe4649d54a104cb46d7f9
ousmane <sangaryousmane@gmail.com>
2023-09-15 01:34:31 +0800

    Add navbar, forms and registrations

--- website/views.py
+++ website/views.py
@@ -1,5 +1,33 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {})
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+        # Authenticate users
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You've been logged in!")
+            return redirect('home')
+        else:
+            messages.error(request, "There was an error logged in, please try again!")
+            return redirect('home')
+    else:
+        return render(request, 'home.html', {})
+
+
+# logout user
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You have been logged out....")
+    return redirect('home')
+
+
+def register(request):
+    return render(request, 'register.html', {})
