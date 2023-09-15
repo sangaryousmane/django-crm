4cbcb252e719c1517473ad560919839ae88ee938
ousmane <sangaryousmane@gmail.com>
2023-09-15 22:14:03 +0800

    Add bunch of data from mockaroo to populate the frontend table

--- website/views.py
+++ website/views.py
@@ -1,8 +1,14 @@
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
-from .models import Record
+
 from website.forms import SignUpForm
+from .models import Record
+
+
+# Checks for authenticated users
+def is_auth(request) -> bool:
+    return request.user.is_authenticated
 
 
 # Create your views here.
@@ -50,3 +56,26 @@ def register(request):
         form = SignUpForm()
         return render(request, 'register.html', {'form': form})
     return render(request, 'register.html', {'form': form})
+
+
+# Get a single customer record
+def customer_records(request, pk: int):
+    if is_auth(request):
+        # Look up Records
+        customer_record = Record.objects.get(id=pk)
+        return render(request, 'record.html', {'customer_record': customer_record})
+    else:
+        messages.error(request, "you must be logged in to view that page")
+        return redirect('home')
+
+
+# Delete a customer record
+def delete_record(request, pk: int):
+    if is_auth(request):
+        deleted = Record.objects.get(id=pk)
+        deleted.delete()
+        messages.success(request, "Record deleted successfully")
+        return redirect('home')
+    else:
+        messages.error(request, "you must be logged in to perform this action")
+        return redirect('home')
